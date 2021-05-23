3ac831e1ba04f296a61f4d661946fb50f14a6c22
Joohny007 <joan.gonzalez.gravi1b@gmail.com>
2021-05-23 20:11:30 +0200

    cielo

--- TJE_framework_2020/src/game.cpp
+++ TJE_framework_2020/src/game.cpp
@@ -64,7 +64,7 @@ Game::Game(int window_width, int window_height, SDL_Window* window)
 	//texture = Texture::Get("data/texture.tga");
 	//mesh = Mesh::Get("data/GiantGeneralPack/Animals_T/penguin_20.obj");
 	//mesh2 = Mesh::Get("data/bloquePrueba4.obj");
-
+	world.inicializeSky();
 	world.inicializePenguins();
 	world.inicializeBlocks();
 	world.inicializePlayer();
